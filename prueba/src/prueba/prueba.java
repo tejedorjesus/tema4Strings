cca0168c0f508baa33d23ed92dd11c201be392a0
Jesus <jesus.tejedor.ovejas@gmail.com>
2017-11-07 17:49:50 +0100

    11/7/2017

--- prueba/src/prueba/prueba.java
+++ prueba/src/prueba/prueba.java
@@ -11,8 +11,8 @@ public class prueba {
 
 		//ejercicio 7 programa que de una frase nos diga mayusculas minusculas y numeros que tiene*/
 
-		
-		
+
+
 
 		int canta=0;
 		int cante=0;
@@ -20,9 +20,9 @@ public class prueba {
 		int canto=0;
 		int cantu=0;
 		int max=0;
-		
-		
-		
+
+
+
 		int k;
 
 		String frase;
@@ -34,62 +34,82 @@ public class prueba {
 		for(k=0; k< frase.length(); k++){
 			if(frase.charAt(k)== 'a'){
 				canta ++;
-				
 				if (canta>max){
 					max=canta;
 				}//if
 			}//if
-		}
-			for(k=0; k< frase.length(); k++){
-			 if(frase.charAt(k)== 'e'){
-					cante ++;
-					if (cante>max){
-						cante= max;
-					}//if
-			 }//if
-			}
-			for(k=0; k< frase.length(); k++){
-					 if(frase.charAt(k)== 'i'){
-						canti ++;
-						if (canti>max){
-							canti=max;
-						}//if
-					 }//if
-			}
-					 for(k=0; k< frase.length(); k++){
-						 if(frase.charAt(k)== 'o'){
-							canto ++;
-							if (canto>max){
-								canto=max;
-							}//if
-						 }//if
-					 }
-					 for(k=0; k< frase.length(); k++){
-							if(frase.charAt(k)== 'u'){
-								cantu ++;
-								if (cantu>max){
-									cantu=max;
-								}//if
-							}//if
-							
-					}//end for
 
-		
+
+			if(frase.charAt(k)== 'e'){
+				cante ++;
+				if (cante>max){
+					max=cante;
+				}//if
+			}//if
+
+
+			if(frase.charAt(k)== 'i'){
+				canti ++;
+				if (canti>max){
+					max=canti;
+				}//if
+			}//if
+
+			if(frase.charAt(k)== 'o'){
+				canto ++;
+				if (canto>max){
+					max=canto;
+				}//if
+			}//if
+
+			if(frase.charAt(k)== 'u'){
+				cantu ++;
+				if (cantu>max){
+					max=cantu;
+				}//if
+			}//if
+
+		}//end for
+
+		if (max==canta){
 			
-		
-	
-		
-		
-	
+		System.out.println( "la letra que mas se repite es la A "+ max +  "  veces " );
+	}
 
-		System.out.println( + max +  "  veces la frase tiene " + canta + " a's " + cante + " e's " + "    " + canti + " i`s"+  "   " +canto + " o's " + "    " + cantu + " u`s");
+		if (max==cante){
+			
+			System.out.println( "la letra que mas se repite es la E "+ max +  "  veces " );
+		}
+
+		if (max==canti){
+			
+		System.out.println( "la letra que mas se repite es la I "+ max +  "  veces " );
+	}
 
 
-						}
-						}
-		
+
+		if (max==canto){
+			
+		System.out.println( "la letra que mas se repite es la O "+ max +  "  veces " );
+	}
+
+
+		if (max==cantu){
+			
+		System.out.println( "la letra que mas se repite es la U "+ max +  "  veces " );
+	}
 
 
 
-	
-		
+
+		System.out.println( " La frase tiene " + canta + " A's " + cante + " E's " + "    " + canti + " I`s"+  "   " +canto + " O's " + "    " + cantu + " U's");
+
+
+	}
+}
+
+
+
+
+
+
